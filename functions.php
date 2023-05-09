b2c728ac183143b85ce6a248f234a88864265169
Herry Prasetyo <herry081288@gmail.com>
2023-05-09 09:10:38 +0700

    first commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+// $conn = new mysqli("localhost", "root", "", "dbpos");
+// if ($conn->connect()) {
+//     echo   "Connected";
+// }else{
+//     echo "Unable to connect";
+// }
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "dbpos");
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Point of Sales</title>
+    <link rel="stylesheet" href="assets/bootstrap5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Halaman Utama</h1>
+    <script src="assets/bootstrap5/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libraries/fontawesome-free/js/all.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+</html>
