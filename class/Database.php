fda9a2dde116939f3718552ae868a3a3d066a470
Herry Prasetyo <herry081288@gmail.com>
2023-05-09 09:49:50 +0700

    changeCode

---
+++ class/Database.php
@@ -0,0 +1,15 @@
+<?php 
+class Database{
+    private static $hostname = "localhost";
+    private static $username = "root";
+    private static $password = "";
+    private static $database    = "";
+
+    protected $db; 
+    
+    public function __construct(){
+        $this->db = mysqli_connect($this->hostname, $this->username, $this->password, $this->database);
+    }
+
+}
+?>
