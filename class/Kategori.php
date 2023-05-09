b7be6d24ab71ba88afa2e35ec2a3b8b3d055e475
Herry Prasetyo <herry081288@gmail.com>
2023-05-09 10:43:06 +0700

    KategoriChange

--- class/Kategori.php
+++ class/Kategori.php
@@ -2,9 +2,20 @@
 include_once ("Database.php");
 
 class Penerbit extends Database {
-    public function all(){
+    //function get all kategori 
+    public function all()
+    {
         $sql = "SELECT * FROM kategori";
         return $this->db->query($sql)->fetch_all(MYSQLI_ASSOC);
+    }
+
+    //function create kategori
+    public function create($data)
+    {
+        $kode = $data["id_kategori"];
+        $nmkategori = $data["nama_kategori"];
+
+        $sql = "INSERT INTO kategori('id_kategori', 'nama_kategori') VALUES ('$kode', '$nmkategori')";
     }
 }
 ?>
